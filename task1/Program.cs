9450fa4f46b1482fe29237d07e5e56bc6c9c4cb2
Pavel <rut100500@yandex.ru>
2022-11-28 22:05:34 +0300

    двумерная мартица

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// вывести двумерную матрицу
+
+// String.Empty - инициализация пустой строки
+// table[0, 0]  table[0, 1]  table[0, 2]  table[0, 3]
+// table[1, 0]  table[1, 1]  table[1, 2]  table[1, 3]
+
+int[,] matrix = new int[3,4];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+Console.WriteLine();
+}
